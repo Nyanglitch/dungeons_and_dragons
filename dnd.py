14bef1242bae444b42a154719e55c2405a29d674
Nyanglitch <kittysaymeow2000@gmail.com>
2020-06-24 16:41:35 -0700

    DnD 1.1 -- play again question asked.

--- dnd.py
+++ dnd.py
@@ -1,4 +1,5 @@
 import random
+import re
 
 print("Welcome to DUNGEONS AND DRAGONS")
 print("You see two caves in front of you.\nOne has treasures, other - certain death!")
@@ -8,6 +9,7 @@ counter = 0
 def pick_destiny(counter):
 
 	user_input = 0
+	continue_input = 'u'
 
 	while int(user_input) not in [1, 2]:
 		print("Which cave you'll pick? (1/2): ")
@@ -20,6 +22,11 @@ def pick_destiny(counter):
 		counter = counter + 1
 		pick_destiny(counter)
 	else:
-		print("You died! Lucky caves: ", counter)
+		print("You died! Lucky caves:", counter)
+		while continue_input.lower() not in ['n', 'y']:
+			continue_input = input("Play again? (y/n): ").lower()
+		if continue_input == 'y':
+			counter = 0
+			pick_destiny(counter)
 
 pick_destiny(counter)
