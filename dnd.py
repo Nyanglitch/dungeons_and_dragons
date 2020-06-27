74794db40834fc8582e958904f683f4a517e000a
Nyanglitch <kittysaymeow2000@gmail.com>
2020-06-26 18:47:11 -0700

    DnD 2.1 -- GOLD improved

--- dnd.py
+++ dnd.py
@@ -7,6 +7,7 @@ random_death_phrases = ["fire breath!", "lunar dragon magic!", "pit with sharp s
 
 counter = 0
 gold = 0
+gold_looted = 0
 continue_input = 'y'
 user_input = ""
 allowed_user_inputs = ['1', '2']
@@ -22,8 +23,9 @@ while continue_input == 'y':
 
 	if user_input == lucky_cave:
 		counter += 1
-		gold += random.randint(1, 666)
-		print("Lucky! Gold looted:", gold)
+		gold_looted = random.randint(1, 666)
+		gold += gold_looted
+		print("Lucky! Gold looted:", gold_looted)
 	else:
 		print("You died from " + random_death_phrases[random.randrange(0, len(random_death_phrases))] + "\nLucky caves:", counter, "\nGold you looted and lost:", gold)
 		continue_input = input("Play again? (y/n): ").lower()
